66f4353fbf179305d9ccbc8fea4dd76a583e9caf
leibo-hust <leibo@hust.edu.cn>
2021-01-21 15:46:31 +0800

    original

---
+++ build/CMakeFiles/dramsim3.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lb/DRAMsim3/src/bankstate.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/bankstate.cc.o"
+  "/home/lb/DRAMsim3/src/channel_state.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/channel_state.cc.o"
+  "/home/lb/DRAMsim3/src/command_queue.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/command_queue.cc.o"
+  "/home/lb/DRAMsim3/src/common.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/common.cc.o"
+  "/home/lb/DRAMsim3/src/configuration.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/configuration.cc.o"
+  "/home/lb/DRAMsim3/src/controller.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/controller.cc.o"
+  "/home/lb/DRAMsim3/src/dram_system.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/dram_system.cc.o"
+  "/home/lb/DRAMsim3/src/hmc.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/hmc.cc.o"
+  "/home/lb/DRAMsim3/src/memory_system.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/memory_system.cc.o"
+  "/home/lb/DRAMsim3/src/refresh.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/refresh.cc.o"
+  "/home/lb/DRAMsim3/src/simple_stats.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/simple_stats.cc.o"
+  "/home/lb/DRAMsim3/src/timing.cc" "/home/lb/DRAMsim3/build/CMakeFiles/dramsim3.dir/src/timing.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FMT_HEADER_ONLY=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ext/headers"
+  "../ext/fmt/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
